9c927b4c37b45e946ee17ee09ab2dd9efa9d21c8
murph2m <murph2m@gmail.com>
2010-11-22 22:08:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MattsApp::Application.config.secret_token = 'deed8ee1972ef5496897da5f6abc3096c9a84649d65385bde5cf71a0d0f754368d49d17f31d302a0a5983766a452d8ca62d66e72a6c30d63816d73238665b59d'
